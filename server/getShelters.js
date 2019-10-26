c15b819b339381fb93521dc8404ea2dbdb1c73c8
Charles Burgess <consulting@cvburgess.com>
2019-10-26 12:41:30 -0400

    add first try at API logic

--- server/getShelters.js
+++ server/getShelters.js
@@ -4,7 +4,12 @@ const pg = require("pg-promise")();
 const db = pg(process.env.DB_URL);
 
 exports.handler = async (event, context) => {
-  const shelters = await db.any("SELECT * FROM shelter WHERE is_complete = false");
+  const { lat, lon } = event.queryStringParameters;
+
+  const shelters = await db.any(
+    "SELECT * FROM shelter ORDER BY coordinates <-> ST_MakePoint(${lat}, ${lon})::geography",
+    { lat, lon }
+  );
 
   return {
     statusCode: 200,
